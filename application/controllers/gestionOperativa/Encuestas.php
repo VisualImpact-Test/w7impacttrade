504c761e1e55e1fdf835cb8d5c7a5fe91a165a00
Harry Pineda <rydak@outlook.com>
2022-01-27 10:29:48 -0500

    Reporte de encuestas con PDF

---
+++ application/controllers/gestionOperativa/Encuestas.php
@@ -0,0 +1,310 @@
+<?
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Encuestas extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('gestionOperativa/M_encuestas', 'model');
+    }
+
+    public function index()
+    {
+        $this->aSessTrack[] = ['idAccion' => 4];
+
+        $config['nav']['menu_active'] = '143';
+        $config['css']['style'] = [
+            'assets/custom/css/asistencia'
+        ];
+        $config['js']['script'] = [
+            'assets/libs/fileDownload/jquery.fileDownload',
+            'assets/libs/datatables/responsive.bootstrap4.min',
+            'assets/custom/js/core/datatables-defaults',
+            'assets/custom/js/core/anyChartCustom',
+            'assets/custom/js/gestionOperativa/encuestas'
+        ];
+        $config['data']['icon'] = 'fal fa-file-alt';
+        $config['data']['title'] = 'Encuestas';
+        $config['data']['message'] = 'Aquí encontrará datos de las encuestas.';
+        $config['view'] = 'modulos/gestionOperativa/encuestas/index';
+        $config['data']['tiposPregunta'] = $this->model->getTiposDePregunta()['query']->result_array();
+
+        $params = [];
+        $params['idCuenta'] = $this->session->userdata('idCuenta');
+        $config['data']['encuestasActivas'] = $this->model->getEncuestasActivas($params)['query']->result_array();
+
+        $this->view($config);
+    }
+
+    public function getCantidadFotosPorEncuesta()
+    {
+        $result = $this->result;
+        $result['msg']['title'] = 'Encuestas';
+        $post = json_decode($this->input->post('data'), true);
+
+        if (isset($post['idEncuesta'])) {
+            $encuestas = $post['idEncuesta'];
+            if (is_array($encuestas)) {
+                $post['idEncuesta'] = implode(",", $encuestas);
+            } else {
+                $post['idEncuesta'] = $encuestas;
+            }
+        }
+
+        $params = [];
+        $dataParaVista = [];
+
+        $params['idCuenta'] = empty($post['cuenta_filtro']) ? "" : $post['cuenta_filtro'];
+        $params['idProyecto'] = empty($post['proyecto_filtro']) ? "" : $post['proyecto_filtro'];
+        $params['idGrupoCanal'] = empty($post['grupo_filtro']) ? "" : $post['grupo_filtro'];
+        $params['subcanal'] = empty($post['subcanal_filtro']) ? "" : $post['subcanal_filtro'];
+        $params['idCanal'] = empty($post['canal_filtro']) ? "" : $post['canal_filtro'];
+        $params['idEncuesta'] = empty($post['idEncuesta']) ? "" : $post['idEncuesta'];
+        $params['tipoPregunta'] = empty($post['tipoPregunta']) ? "" : $post['tipoPregunta'];
+        $params['txt-fechas'] = empty($post['txt-fechas']) ? "" : $post['txt-fechas'];
+
+        $params['distribuidora_filtro'] = empty($post['distribuidora_filtro']) ? '' : $post['distribuidora_filtro'];
+        $params['zona_filtro'] = empty($post['zona_filtro']) ? '' : $post['zona_filtro'];
+        $params['plaza_filtro'] = empty($post['plaza_filtro']) ? '' : $post['plaza_filtro'];
+        $params['cadena_filtro'] = empty($post['cadena_filtro']) ? '' : $post['cadena_filtro'];
+        $params['banner_filtro'] = empty($post['banner_filtro']) ? '' : $post['banner_filtro'];
+
+        $params['fecIni'] = getFechasDRP($post["txt-fechas"])[0];
+        $params['fecFin'] = getFechasDRP($post["txt-fechas"])[1];
+
+        $data = $this->model->getCantidadFotosPorEncuesta($params);
+        $dataFotos = $this->model->getTotalFotosPorEncuesta($params);
+
+        $maximoDeColumas = 0;
+        foreach ($dataFotos['query']->result_array() as $key => $value) {
+            $dataParaVista['fotosEncuesta'][$value['idVisitaEncuesta']][$value['idVisitaFoto']] = $value['fotoUrl'];
+        }
+        if (!empty($dataParaVista['fotosEncuesta'])) {
+            foreach ($dataParaVista['fotosEncuesta'] as $key => $value) {
+                $maximoDeColumas = max(count($value), $maximoDeColumas);
+            }
+        }
+        $dataParaVista['maximoDeColumas'] = $maximoDeColumas;
+
+        $dataParaVista['visitas'] = $data['query']->result_array();
+
+        $result['result'] = 1;
+        if ($data['estado'] == false || empty($dataParaVista['visitas'])) {
+            $result['data']['html'] = getMensajeGestion('noRegistros');
+        } else {
+            $segmentacion = getSegmentacion(['grupoCanal_filtro' => $params['idGrupoCanal']]);
+            $dataParaVista['segmentacion'] = $segmentacion;
+            $result['data']['html'] = $this->load->view("modulos/gestionOperativa/Encuestas/tablaDetalladoEncuesta", $dataParaVista, true);
+            $result['data']['configTable'] = [];
+        }
+
+        echo json_encode($result);
+    }
+
+    public function getFotosPorEncuesta()
+    {
+        $result = $this->result;
+        $result['msg']['title'] = 'Fotos de Encuestas';
+        $post = json_decode($this->input->post('data'), true);
+
+        $data = $this->model->getFotosPorEncuesta($post);
+
+        $dataParaVista['fotos'] = $data['query']->result_array();
+
+        $result['result'] = 1;
+        if ($data['estado'] == false || empty($dataParaVista['fotos'])) {
+            $result['data']['html'] = getMensajeGestion('noRegistros');
+        } else {
+            $result['data']['html'] = $this->load->view("modulos/gestionOperativa/Encuestas/formularioFotos", $dataParaVista, true);
+        }
+
+        echo json_encode($result);
+    }
+
+    function generar_pdf()
+    {
+        $dataLista = json_decode($this->input->post('data'));
+
+        header('Set-Cookie: fileDownload=true; path=/');
+        header('Cache-Control: max-age=60, must-revalidate');
+
+        $www = base_url() . 'public/assets/images';
+        $archivo = 'MODULO DE ENCUESTAS - REPORTE DE FOTOS SELECCIONADAS.pdf';
+
+        $style = "
+            <style>
+                .divImagen{
+                    margin: 5px !important;
+                }
+                img.foto{ 
+                    border: 10px solid #848484; 
+                }
+                .head {
+                    background-color:#c00002;
+                    padding: 5px;
+                    font-family: Lucida Grande,Lucida Sans,Arial,sans-serif;
+                    font-size: 10px;
+                    width: 100%;
+                }
+                .tdCuenta{
+                    display: inline-flex;
+                }
+                .cuenta{
+                    font-size: 20px;
+                    color: #FFFFFF !important;
+                    font-weight: bold;
+                    text-align: left !important;
+                }
+                .espacio{
+                    width: 140px !important;
+                }
+                .logo { height: 40px; }
+                .logo_anio { height: 50px !important; }
+                .title { font-weight: bold; color: #FFFFFF !important; text-align: right; }
+                .table-item { 
+                    margin-top: 20px !important; 
+                    font-family: Lucida Grande,Lucida Sans,Arial,sans-serif;
+                    font-size: 14px;
+                    width: 100%;
+                }
+                /*.table-item td{
+                    border: 1px solid #f00; 
+                }*/
+                .subTitle {
+                    font-weight: bold;
+                    color: #FFFFFF !important;
+                    background-color: #848484;
+                    text-align: center !important;
+                }
+                .tdNombreEncuesta{
+                	height: 80px !important;
+                    background-color: #ffc000;
+                    color:  #000000;
+                    width: 100%;
+                    text-align: left;
+                    font-size: 30px !important;
+                    margin-top: 50px !important;
+                    margin-bottom: 50px !important;
+                }
+                .tdNombreTienda{
+                	height: 50px !important;
+                    text-align:center; 
+                    width: 100%;
+                    font-size: 30px !important;
+                    background-color: #bc3931;
+                    color: #FFFFFF;
+                    margin-top: 20px;
+                    margin-bottom: 20px;
+                }
+                .tdFoto{
+                    padding: 15px !important;
+                }
+            </style>
+            ";
+
+        require APPPATH . '/vendor/autoload.php';
+
+        ini_set('memory_limit', '1024M');
+        set_time_limit(0);
+
+        $mpdf = new \Mpdf\Mpdf();
+        $printHtml = '';
+
+        foreach ($dataLista as $klc => $cliente) {
+            $nombreEncuesta = $cliente->{'nombreEncuesta'};
+            $nombreCliente = $cliente->{'nombreCliente'};
+            $cantidadFotos = $cliente->{'cantidadFotos'};
+
+            //Creamos la Cabecera
+            $header = '<table class="head" >';
+            $header .= '<tr>';
+            $header .= '<td>';
+            $header .= '<img class="logo" src="' . $www . '/visualimpact/logo.png" />';
+            $header .= '</td>';
+            $header .= '<td></td>';
+            $header .= '<td class="title" >';
+            $header .= 'Fecha Creación: ' . date('d/m/Y');
+            $header .= '</td>';
+            $header .= '</tr>';
+            $header .= '</table>';
+
+            $htmlCabecera = '';
+            $htmlCabecera .= '<div style="margin:30px;display:table;">&nbsp;</div>';
+            $htmlCabecera .= '<table class="table-item" >';
+            $htmlCabecera .= '<tbody>';
+
+            $htmlCabecera .= '<tr>';
+            $htmlCabecera .= '<td class="tdNombreEncuesta">';
+            $htmlCabecera .= '<div style="margin-top: 50px !important;">';
+            $htmlCabecera .= '<strong> ' . $nombreEncuesta . ' </strong> <br />';
+            $htmlCabecera .= '</div>';
+            $htmlCabecera .= '</td>';
+            $htmlCabecera .= '</tr>';
+            $htmlCabecera .= '<tr>';
+            $htmlCabecera .= '<td class="tdNombreTienda">';
+            $htmlCabecera .= '<strong> ' . $nombreCliente . '</strong><br />';
+            $htmlCabecera .= '</td>';
+            $htmlCabecera .= '</tr>';
+            $htmlCabecera .= '<tr>';
+            $htmlCabecera .= '<td>&nbsp;</td>';
+            $htmlCabecera .= '</tr>';
+
+            /**====AQUI EL CONTENIDO======**/
+
+            $htmlFooter = '';
+            $htmlFooter .= '<tr>';
+            $htmlFooter .= '<td>&nbsp;</td>';
+            $htmlFooter .= '</tr>';
+            $htmlFooter .= '</tbody>';
+            $htmlFooter .= '</table>';
+
+            if (!empty($cliente->dataFotos)) {
+                foreach ($cliente->dataFotos as $klc => $arrayFotos) {
+                    if (!empty($arrayFotos)) {
+
+                        $htmlFoto = '';
+                        $htmlFoto .= '<tr><td>';
+                        $htmlFoto .= '<table>';
+                        $contTdFotos = 0;
+                        foreach ($arrayFotos as $kf => $fotoImage) {
+                            if ($contTdFotos == 0) {
+                                $htmlFoto .= '<tr>';
+                            }
+
+                            $htmlFoto .= '<td class="tdFoto" style="width:50% !important;">';
+                            $htmlFoto .= '<img class="foto" src="http://movil.visualimpact.com.pe/fotos/impactTrade_android/encuestas' . $fotoImage . '" />';
+                            $htmlFoto .= '</td>';
+
+                            $contTdFotos++;
+
+                            if ($contTdFotos % 2 == 0) {
+                                $htmlFoto .= '</tr>';
+                                $contTdFotos = 0;
+                            }
+                        }
+                        if ($contTdFotos == 1) {
+                            $htmlFoto .= '<td class="tdFoto" style="width:50% !important;"></td>';
+                            $htmlFoto .= '</tr>';
+                        }
+
+                        $htmlFoto .= '</table>';
+                        $htmlFoto .= '</td></tr>';
+
+
+                        $mpdf->SetHTMLHeader($header);
+                        $mpdf->setFooter('{PAGENO}');
+                        $mpdf->AddPage();
+                        $mpdf->WriteHTML($style);
+                        $mpdf->WriteHTML($htmlCabecera);
+                        $mpdf->WriteHTML($htmlFoto);
+                        $mpdf->WriteHTML($htmlFooter);
+                    }
+                }
+            }
+        }
+
+        $mpdf->Output($archivo, 'D');
+    }
+}
---
+++ application/views/modulos/GestionOperativa/Encuestas/index.php
@@ -0,0 +1,157 @@
+<div class="row mt-4">
+	<div class="col-lg-2 d-flex justify-content-center align-items-center">
+		<h3 class="card-title mb-3">
+			<i class="<?= $icon ?>"></i>
+			<?= $title ?>
+		</h3>
+	</div>
+	<div class="col-lg-10 d-flex">
+		<div class="card w-100 mb-3 p-0">
+			<div class="card-body p-0">
+				<ul class="nav nav-tabs nav-justified">
+					<li class="nav-item btnReporte" id="tipoReporte" name="tipoReporte" data-value="1"><a data-toggle="tab" href="#idDetalleHorizontal" class="active nav-link" data-url="getCantidadFotosPorEncuesta" data-content="idDetalleHorizontal" data-tabla="tablaDetalladoEncuesta">Detallado</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="customizer border-left-blue-grey border-left-lighten-4 d-none d-xl-block">
+	<a href="javascript:;" class="customizer-close"><i class="fal fa-times"></i></a>
+	<a href="javascript:;" class="customizer-toggle box-shadow-3 bg-trade-visual-grad-left text-white">
+		<i class="fal fa-cog fa-lg fa-spin"></i>
+	</a>
+	<div class="customizer-content p-2 ps-container ps-theme-dark" data-ps-id="aca1f25c-4ed9-a04b-d154-95a5d6494748">
+		<form id="formFiltrosEncuesta">
+			<div class="card-header" style="margin-bottom: 14px;">
+				<h4>CONFIGURACIÓN</h4>
+			</div>
+			<div>
+				<input type="hidden" id="idTipoFormato" name="tipoFormato" value="1">
+			</div>
+			<div class="customizer-content-button">
+				<button type="button" class="btn btn-outline-trade-visual border-0" data-url="filtrar" id="btn-filtrarEncuesta" title="Filtrar">
+					<i class="fa fa-search"></i> <span class="txt_filtro"></span>
+				</button>
+				<button type="button" class="btn btn-outline-trade-visual border-0" data-url="pdf" id="btn-encuesta-pdf" title="PDF">
+					<i class="fa fa-file-pdf"></i> <span class="txt_filtro"></span>
+				</button>
+			</div>
+			<hr>
+			<div class="customizer-content-filter">
+				<h5 class="mt-1 mb-1 text-bold-500"><i class="fal fa-table"></i> Filtros</h5>
+				<div class="form-row">
+					<div class="col-md-12">
+						<!-- <div class="mb-2 mr-sm-2 position-relative form-group">
+							<label>Ver Fotos:</label><br>
+							<div role="group" class="btn-group btn-group-toggle" data-toggle="buttons">
+								<label class="btn btn-outline-trade-visual border-0 active">
+									<input type="radio" name="verFotos" value="false" checked="checked">
+									No
+								</label>
+								<label class="btn btn-outline-trade-visual border-0">
+									<input type="radio" name="verFotos" value="true">
+									Sí
+								</label>
+							</div>
+						</div> -->
+						<div class="mb-2 mr-sm-2 position-relative form-group">
+							<label for="idEncuesta">Encuestas Activas:</label>
+							<div class="col-xs-10">
+								<select class="selectpicker ui my_select2Full" id="idEncuesta" name="idEncuesta" title="Encuestas (Todo)" data-live-search="true">
+									<?php foreach ($encuestasActivas as $row) { ?>
+										<option class="dropdown-item" value="<?= $row['idEncuesta'] ?>"><?= $row['encuesta'] ?></option>
+									<? } ?>
+								</select>
+							</div>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group">
+							<div class="field">
+								<div class="ui my_calendar">
+									<div class="ui input left icon" style="width:100%">
+										<i class="calendar icon"></i>
+										<input type="text" name="txt-fechas" id="txt-fechas" placeholder="Date" value="<?= date("d/m/Y") . ' - ' . date("d/m/Y") ?>" class="form-control">
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia">
+							<?= getFiltros(['cuenta' => ['label' => 'Cuenta', 'name' => 'cuenta_filtro', 'id' => 'cuenta_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]) ?>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia">
+							<?= getFiltros(['proyecto' => ['label' => 'Proyecto', 'name' => 'proyecto_filtro', 'id' => 'proyecto_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]) ?>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+							<span class="tooltiptext">Grupo Canal</span>
+							<?= getFiltros(['grupoCanal' => ['label' => false, 'name' => 'grupo_filtro', 'id' => 'grupo_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]) ?>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+							<span class="tooltiptext">Canal</span>
+							<?= getFiltros(['canal' => ['label' => 'Canal', 'name' => 'canal_filtro', 'id' => 'canal_filtro', 'data' => false, 'select2' => 'ui my_select2Full', 'html' => '']]) ?>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+							<span class="tooltiptext">Sub Canal</span>
+							<?= getFiltros(['tipoCliente' => ['label' => 'Sub Canal', 'name' => 'subcanal_filtro', 'id' => 'subcanal_filtro', 'data' => false, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+						</div>
+						<div class="mb-2 mr-sm-2 position-relative form-group custom_tooltip">
+							<span class="tooltiptext">Tipo Pregunta</span>
+							<select id="tipoPregunta" class="ui my_select2Full" name="tipoPregunta">
+								<option value="">-- Tipo Pregunta --</option>
+								<? foreach ($tiposPregunta as $key => $value) { ?>
+									<option value="<?= $value["idTipoPregunta"] ?>"><?= $value["nombre"] ?></option>
+								<? } ?>
+							</select>
+						</div>
+						<div class="filtros_secundarios">
+							<div class="filtros_generados">
+								<h5 class="mt-1 mb-1 text-bold-500"><i class="fal fa-table"></i> Filtros Generados</h5>
+								<div class="filtros_gc filtros_HFS d-none">
+									<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+										<span class="tooltiptext">Distribuidora</span>
+										<?= getFiltros(['distribuidora' => ['label' => 'Distribuidora', 'name' => 'distribuidora_filtro', 'id' => 'distribuidora_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+									</div>
+								</div>
+								<div class="filtros_gc filtros_WHLS d-none">
+									<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+										<span class="tooltiptext">Zona</span>
+										<?= getFiltros(['zona' => ['label' => 'Zona', 'name' => 'zona_filtro', 'id' => 'zona_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+									</div>
+									<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+										<span class="tooltiptext">Plaza</span>
+										<?= getFiltros(['plaza' => ['label' => 'Plaza', 'name' => 'plaza_filtro', 'id' => 'plaza_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+									</div>
+								</div>
+								<div class="filtros_gc filtros_HSM filtros_Moderno d-none">
+									<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+										<span class="tooltiptext">Cadena</span>
+										<?= getFiltros(['cadena' => ['label' => 'Cadena', 'name' => 'cadena_filtro', 'id' => 'cadena_filtro', 'data' => true, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+									</div>
+									<div class="mb-2 mr-sm-2 position-relative form-group filtros_asistencia custom_tooltip">
+										<span class="tooltiptext">Banner</span>
+										<?= getFiltros(['banner' => ['label' => 'Banner', 'name' => 'banner_filtro', 'id' => 'banner_filtro', 'data' => false, 'select2' => 'ui my_select2Full', 'html' => '']]); ?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="ps-scrollbar-x-rail" style="left: 0px; bottom: 3px;">
+				<div class="ps-scrollbar-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+			</div>
+			<div class="ps-scrollbar-y-rail" style="top: 0px; right: 3px;">
+				<div class="ps-scrollbar-y" tabindex="0" style="top: 0px; height: 0px;"></div>
+			</div>
+		</form>
+	</div>
+</div>
+
+<div class="main-card mb-3 card div-para-ocultar">
+	<div class="card-body p-0">
+		<div class="tab-content" id="content-auditoria">
+			<div class="tab-pane fade show active" id="idDetalleHorizontal" role="tabpanel">
+				<?= getMensajeGestion('noResultados') ?>
+			</div>
+		</div>
+	</div>
+</div>
